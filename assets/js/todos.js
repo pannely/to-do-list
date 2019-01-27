17d239384cd7c9bc1732409d4d384fc58e52ce15
pannely <hey.annely@gmail.com>
2019-01-27 03:30:55 -0800

    completed functionality

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,19 +1,22 @@
 //check off specific todos by clicking
-$("li").click(function(){
-	//if li is grey
-	if ($(this).css("color") === "rgb(128, 128, 128)"){
-		//turn it back
-		$(this).css({
-		color: "black",
-		textDecoration: "none" 
-		});	
-	} else {
-		$(this).css({
-			color: "gray",
-			textDecoration: "line-through" /*tricky syntax*/
-		});	
+$("ul").on("click", "li", function(){ //using "on() allows you to add arguments "
+	$(this).toggleClass("completed");
+});
+
+//click on x to delete todo
+$("ul").on("click", "span", function(event){ /*add event object to stop bubbling*/
+	$(this).parent().fadeOut(500, function(){ /*add a function to ensure fadeout before remove*/
+		$(this).remove(); /* this now refers to li */
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		//grabbibg new todo text from input
+		var todoText = $(this).val();
+		$(this).val(""); //clears out input
+		//create a new li and add to ul
+		$("ul").append("<li><span>x</span> "+todoText+"</li>")
 	}
-	//else
-		//turn it grey
-	
 });
